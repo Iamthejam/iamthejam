a4a6b848e0412a753f46ac5fdf3eb1f3ea1399f7
Iamthejam <inkatha2000@gmail.com>
2014-05-02 13:49:31 -0500

    Create photos.php

---
+++ photos.php
@@ -0,0 +1,28 @@
+<?php
+include_once 'header.php';
+?>
+
+<!DOCTYPE div PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<body>
+	<div id="mid-photos">
+		<div id="cp_widget_b5ce4934-5f73-409d-831d-bdec95652738">...</div>
+		<script type="text/javascript">
+var cpo = []; cpo["_object"] ="cp_widget_b5ce4934-5f73-409d-831d-bdec95652738"; cpo["_fid"] = "AcMALorsXF--";
+var _cpmp = _cpmp || []; _cpmp.push(cpo);
+(function() { var cp = document.createElement("script"); cp.type = "text/javascript";
+cp.async = true; cp.src = "//www.cincopa.com/media-platform/runtime/libasync.js";
+var c = document.getElementsByTagName("script")[0];
+c.parentNode.insertBefore(cp, c); })(); </script>
+		<noscript>
+			Powered by Cincopa <a href='http://www.cincopa.com/video-hosting'>Video
+				Hosting</a> solution.
+		</noscript>
+</div>
+</body>
+</html>
+
+
+<?php
+include_once 'footer.php';
+?>
