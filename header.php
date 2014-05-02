f47b056835b2f0df07950b441b9d0eff5c0991f4
Iamthejam <inkatha2000@gmail.com>
2014-05-02 13:48:15 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>I-AM YOUTH PROGRAM</title>
+<meta name="description" content="Description of your site goes here">
+<meta name="keywords" content="keyword1, keyword2, keyword3">
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div id="main-wraper">
+		<div id="top-wraper">
+			<div id="banner">
+				<img src="inkathalogo.png">I-AM YOUTH PROGRAM
+			</div>
+			<div id="nav">
+				<ul>
+					<li><a href="index.php">Home</a>
+					</li>
+					<li><a href="aboutUs.php">About us</a>
+					</li>
+					<li><a href="products.php">Products</a>
+					</li>
+					<li><a href="photos.php">Photos</a>
+					</li>
+					<li><a href="donate.php">Donate</a>
+					</li>
+					<li
+						style="padding: 0px 2px 0px 3px; background: transparent none repeat scroll 0% 50%; -moz-background-clip: initial; -moz-background-origin: initial; -moz-background-inline-policy: initial;"><a
+						href="contactUs.php">Contact Us</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		</div>
