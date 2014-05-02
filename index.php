72db39fa9e6d5065e76f6de2d40e8994d8209f17
Iamthejam <inkatha2000@gmail.com>
2014-05-02 13:48:34 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,109 @@
+<?php 
+include_once 'header.php';
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+	<div id="mid-wraper">
+		<div id="mid-wraper-inner">
+			<div id="left-wraper">
+				<div id="left-wraper-top">
+					<div
+						style="margin: 0px; padding: 20px; width: 256px; float: left; display: block;">
+						<h1>Categories</h1>
+					</div>
+					<ul>
+						<li><a href="#">Link would go here</a></li>
+						<li><a href="#">Link would go here </a></li>
+						<li><a href="#">Link would go here </a></li>
+						<li><a href="#">Link would go here </a></li>
+						<li><a href="#">Link would go here </a></li>
+						<li><a href="#">Link would go here </a></li>
+						<li><a href="#">Link would go here </a></li>
+						<li><a href="#">Link would go here </a></li>
+					</ul>
+				</div>
+				<div id="news">
+					<h3>News</h3>
+					<img src="images/news.jpg" alt="">
+					<div style="width: 270px; float: left; padding-top: 10px;">
+						Thanks to our developers, I-Am Youth Foundation has a new look and feel to our website.  <br>
+						<br> <strong>3.14.2014</strong> News about the Inkatha Youth
+						Foundation would be entered here to inform visitors about upcoming
+						events.<br> <br> <a href="#" class="link"
+							style="float: left;">Read More</a>
+					</div>
+				</div>
+			</div>
+			<div id="right-wraper">
+				<div class="right-part">
+					<h1>
+						Welcome to <span class="gold-text">our website</span>
+					</h1>
+					<img src="images/welcome-brd.jpg" alt="">
+					<div style="padding: 10px 0px; width: 596px; float: left;">
+						This program has been developed by Laurence T. Inkatha, Founder &
+						C.E.O of The Inkatha Youth Foundation for the arts. We are a
+						501(c)(3) State and Federal Tax Exempt Foundation, dedicated to
+						teaching Local Youth the cultural Arts, the Art of Business and
+						Positive Life Skills.<br> <br> <br>
+						<div
+							style="padding: 5px 15px; background: transparent url(images/bullet_point.png) no-repeat scroll 0px 7px; -moz-background-clip: initial; -moz-background-origin: initial; -moz-background-inline-policy: initial; width: 266px; float: left; text-decoration: underline;">Learn
+							about dance</div>
+						<div
+							style="padding: 5px 15px; background: transparent url(images/bullet_point.png) no-repeat scroll 0px 7px; -moz-background-clip: initial; -moz-background-origin: initial; -moz-background-inline-policy: initial; width: 266px; float: left; text-decoration: underline;">Learn
+							about Art</div>
+						<div
+							style="padding: 5px 15px; background: transparent url(images/bullet_point.png) no-repeat scroll 0px 7px; -moz-background-clip: initial; -moz-background-origin: initial; -moz-background-inline-policy: initial; width: 266px; float: left; text-decoration: underline;">Learn
+							about Poetry</div>
+						<div
+							style="padding: 5px 15px; background: transparent url(images/bullet_point.png) no-repeat scroll 0px 7px; -moz-background-clip: initial; -moz-background-origin: initial; -moz-background-inline-policy: initial; width: 266px; float: left; text-decoration: underline;">Learn
+							about Singing</div>
+						<div
+							style="padding: 5px 15px; background: transparent url(images/bullet_point.png) no-repeat scroll 0px 7px; -moz-background-clip: initial; -moz-background-origin: initial; -moz-background-inline-policy: initial; width: 266px; float: left; text-decoration: underline;">Learn
+							about Instruments</div>
+						<div
+							style="padding: 5px 15px; background: transparent url(images/bullet_point.png) no-repeat scroll 0px 7px; -moz-background-clip: initial; -moz-background-origin: initial; -moz-background-inline-policy: initial; width: 266px; float: left; text-decoration: underline;">Learn
+							about Business</div>
+					</div>
+				</div>
+				<div class="right-part" style="padding: 10px 0px;">
+					<div style="width: 290px; float: left;">
+						<h2>
+						About <span class="gold-text">us</span>
+						</h2>
+						<img src="images/small-brd.jpg" alt="" height="3" width="285">
+						<div
+							style="border-right: 1px solid rgb(153, 153, 153); width: 289px; float: left; padding-right: 10px; margin-top: 10px;">
+							Our mission is to provide all Youth with a NEW Positive Life
+							Style through education and real world, profitable experiences.
+							The IAM Youth Program provides organization, inspiration,
+							opportunity and positive avenues of expression of God Given
+							Talents. Local Youth will be empowered with the skills and
+							connections needed to build a NEW POSITVE LIFESTYLE. <br>
+							<a href="aboutUs.php">Read more</a>
+						</div>
+					</div>
+					<div style="width: 270px; float: right; padding-right: 10px;">
+						<h2>
+							Recent <span class="gold-text">articles</span>
+						</h2>
+						<img src="images/small-brd.jpg" alt="" height="3" width="265">
+						<div
+							style="width: 269px; float: left; padding-right: 10px; margin-top: 10px;">
+							"LOVE & UNITY"<br> This is the remedy to our youth problem!
+							Yet, like an ymedical prescription Love & Unity must be
+							administered correctly and in the proper dosage. <br>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	</body>
+	</html>
+	<?php 
+include_once 'footer.php';
+?>
