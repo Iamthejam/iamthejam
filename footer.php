d012f7ad47f0eff0585b82f773d688697e82f1d9
Iamthejam <inkatha2000@gmail.com>
2014-05-02 13:47:56 -0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<body>
+	<div id="foter">
+		<div id="foter-inner">
+			<p
+				style="margin: 0px; padding: 0px; width: 950px; float: left; display: block;">
+				<a href="index.php" class="footer-link">Home</a> 
+				<a href="aboutUs.php" class="footer-link">About us</a> 
+				<a href="products.php" class="footer-link">Products</a>
+				<a href="photos.php" class="footer-link">Photos</a> 
+				<a href="login.php" class="footer-link">Login</a> 
+				<a href="contactUS.php" class="footer-link">Contact Us</a>
+			</p>
+		</div>
+	</div>
+</body>
+</html>
+
