cf3c6b39160125ec535eeeb25de3cfeb1c995408
Iamthejam <inkatha2000@gmail.com>
2014-05-02 13:45:26 -0500

    Create aboutus.php

---
+++ aboutus.php
@@ -0,0 +1,169 @@
+<?php
+include_once 'header.php';
+?>
+<
+<!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<body>
+	<div id="mid-wraper">
+		<img border="0"
+			src="images/iamjambanner.jpg"
+			alt="Banner" width="935" height="228"> #
+		<div id="aboutus">
+			<table>
+				<tr>
+					<td id="about-pics"><img border="0"
+						src="images/hivaward.jpg"
+						alt="Award Letter" width="400" height="228"> <img border="0" src=>
+					</td>
+					<td id="about-text"><p>This program has been developed by Laurence
+							T. Inkatha, Founder & C.E.O of The Inkatha Youth Foundation for
+							the arts. We are a 501(c)(3) State and Federal Tax Exempt
+							Foundation, dedicated to steaching Local Youth the cultural Arts,
+							the Art of Business and Positive Life Skills.</p> <img
+						src="images/small-brd.jpg" alt="" height="3" width="500"> <br> <br>
+						<br>
+
+						<p>Our mission is to provide all Youth with a NEW Positive Life
+							Style through education and real world, profitable experiences.
+							The IAM Youth Program provides organization, inspiration,
+							opportunity and positive avenues of expression of God Given
+							Talents. Local Youth will be empowered with the skills and
+							connections needed to build a NEW POSITVE LIFESTYLE.</p> <img
+						src="images/small-brd.jpg" alt="" height="3" width="500"> <br> <br>
+						<br>
+						<p>
+							"LOVE" THE PRINCIPLE TO PEACE:<br> Gang members "LOVE" their
+							family members and fellow gang members. Most people think that it
+							is the power of hatred that is causing our children to kill each
+							other. I beg to dffer with this ans offer that it is the
+							"Mis-Guided Power of LOVE". Most of our youth do not know the
+							rival gang members they are shooting at well enough to have
+							developed "HATE" for the person, only hate for the rival gang
+							they represent.It is the "LOVE" the youth has forhis/her fellow
+							"gang member" or "family member" that fuels the emotion of
+							revenge and makes the gang member want to take violent action. It
+							is the "LOVE" of the brotherhood/Sisterhood that fuels the
+							"HATRED"! We must use that same emotion of "LOVE" to bring about
+							PEACE!
+						
+						<p>
+							<img src="images/small-brd.jpg" alt="" height="3" width="500"> <br>
+							<br> <br>
+						
+						<p>
+							"UNITY"- PRINCIPLE TO SUCCESS:<br> If you want to stop a Bad
+							Gang, one way is to build a Bigger and Better Gang. The Bigger &
+							Better Gang must provide a new standard of:<br>"COMMUNITY UNITY &
+							POSITIVE YOUTH SERVICE"!
+						</p> <img src="images/small-brd.jpg" alt="" height="3" width="500">
+
+						<br> <br> <br>
+
+						<p>
+							MATCHING INCINTIVE:<br> We must provide our youth with at least
+							the same service the gangs offr them:<br> 1. Some money in their
+							pockets<br> 2. A special FAMILY to beling to.
+						</p> <img src="images/small-brd.jpg" alt="" height="3" width="500">
+						<br> <br> <br>
+						<p>
+							MEDIATION PRINCIPLE #1: "A PIECE OF THE ACTION": <br> The history
+							of American Gang Warfare show us that the best way to come to an
+							agreement and bring about PEACE is for one gang to give up some
+							territoryand money making opportunity to the opposing gang.
+							Ouryouth does not have any real ownership in our communities.
+							This has to change. Once our youth gain opportunities for
+							community ownership and development there will be PEACE.
+						</p> <img src="images/small-brd.jpg" alt="" height="3" width="500">
+						<br> <br> <br>
+
+						<p>
+							MEDIATION PRINCIPLE #2:<br> "LOVE FOR THEIR YOUNGER FAMILY
+							MEMBER"<br> <br> Unless the Gang Member has totally lost his/her
+							mind, any real gang member with real street experience DOES NOT
+							want their younger family member to have to live that gang/street
+							lifestyle. This love for their younger family member opens the
+							door for cooperation between us and the hard core gang member.
+							But it means nothing unless we provide the opportunitiy for a
+							positive and progressive TOTALLY NEW YOUTH LIFESTYLE!
+						</p> <img src="images/small-brd.jpg" alt="" height="3" width="500">
+						<br> <br> <br>
+						<p>
+							"CREATING THE I-AM FAMILY"<br> <br> We must recruit 1000 Local
+							Community People, Businesses, Churches or Organizations that will
+							donate $20.00 per month for one year. We call out sponsors "I-AM
+							ANGELS"! This will give us a monthly budget of $20,000 per month,
+							for one year. Your $20.00 donation will be distributed as
+							follows:<br> <br> $10.00 Per Month goes directly to the pocket of
+							the I-AM Youth Member. Our Goal is to identify 100 Local Youth
+							who have Entreprenurial Talentthat we can help develop. When our
+							I-AM Youth Member has 10 I-AM Angels (Sponsors) who provide
+							him/her with $10.00 per month, that youth now has an account
+							budget worth $100.00 per month for on year. Collectively the 100
+							I-AM's have a monthly budget of $10,000 Per Month.
+						</p> <img src="images/small-brd.jpg" alt="" height="3" width="500">
+						<br> <br> <br>
+						<p>
+							I-AM stands for INKATHA ACCOUNT MANAGER, $5.00 goes to pay for
+							FREE I-AM Cultural Arts classes that must produce a Marketable
+							Product.<br> <br> EXAMPLE: "I-AM Poetry Class".<br> <br> We pay
+							for the teacher to teach a two hour class for 10 weeks. We pay
+							for the room space and all the supplies necessary for 20 students
+							to go through the course. Our classes meet one day per week,two
+							hours per dayfor 10 weeks in a row. Let's say every Tuesday from
+							6pm--8pm. Over the 10 weeks. We will take the 100 poems and
+							produce and publish a book for "I-AM POETRY CLASS #1"<br> <br>
+							Now each student is a published poet. We do the same with
+							singers, Rappers, Dancers, Actors,Drawing, Writting, Designing,
+							Sweing, Photography etc. When we announce the starting date for a
+							class, Students are chosen on a first come basis. Class Budhet =
+							$5000.00 Per Month $5.00 goes to pay I-AM Foundation Expenses.
+							Staff Salaries, Facility Rentals, Product Production, etc.
+							Expenses budget = $5000.00 per month.
+						</p> <img src="images/small-brd.jpg" alt="" height="3" width="500">
+						<br>
+					<br>
+					<br>
+						<p>
+							"ENGAGING OUR LOCAL YOUTH"<br>
+							<br> "I-AM BUSINESS CLASS TV & RADIO ANNOUNCEMENT"<br>
+							<br> Once we acheive the goal of signing up the first 500 Local
+							I-AM Angels we will gather the 500 people together, wearing our
+							shirts & hats, to produce a public service TV & Radio
+							announcement stating the following:<br>
+							<br> "are you between the ages of 8 and 18? Do you have talent
+							for business?"<br>
+							<br> Would you like to earn a minimum of $100.00 per month for
+							the next 12 months?<br>
+							<br> We are announcing the opening of "The I-Am business class!"
+							To graduate our 10 week class you must recruit 10 people who will
+							sponsor you for $20.00 per month. (As the camera pulls back to
+							reveal the 500 I-Am Angels) But wait... we already have 500
+							people who have already signed up to sponsor you for $20.00 per
+							month for one year. We will give you one sponsor for every
+							sponsor you sing up. So to graduate our class all you need to do
+							is find 5 sponsors to graduate with 10. You can have as many
+							Sponsors as you caqn recruit. Come on and challenge: THE I-AM
+							BUSINESS CLASS (WE ALL SAY)"I-AM WAITING FOR YOU"
+						</p>
+						<img src="images/small-brd.jpg" alt="" height="3" width="500"></td>
+
+
+
+
+				</tr>
+
+			</table>
+
+		</div>
+
+</body>
+
+</html>
+
+
+
+<?php
+include_once 'footer.php';
+?>
+					
